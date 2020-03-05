4a8a50d8c7aab30591096c43d7463210658e8195
Hanniee Tran <54680570+hanniee227@users.noreply.github.com>
2020-03-05 09:12:59 -0500

    MainMenu/PauseMenu Added

---
+++ FinalProject-CouchPotatoStudio/Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public static bool winLoseCondition = false;
+    public GameObject pauseMenuUI;
+
+    private bool optionOn = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && winLoseCondition == false)
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else if (optionOn == false)
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; //unfreeze the game
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; //freeze the game
+        GameIsPaused = true;
+    }
+
+    public void LoadMainMenu()
+    {
+        //Debug.Log("Loading Menu");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quitting game from Hub");
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("HUB");
+        Time.timeScale = 1f; //unfreeze the game
+    }
+
+    public void optionMenu()
+    {
+        optionOn = true;
+    }
+
+    public void backButton()
+    {
+        optionOn = false;
+    }
+}
---
+++ FinalProject-CouchPotatoStudio/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Time.timeScale = 1f; //unfreeze the game (incase they decided to play AFTER going main menu from pause menu)
+        SceneManager.LoadScene("HUB");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting From Main");
+        Application.Quit();
+    }
+}
