779fc5fb240021da1f7912a3a29f42a5ab207aa3
virt-x <xvirt@outlook.com>
2020-04-05 08:26:30 -0400

    lots of stuff
    I added options to the settings menu, including volume controls, options to invert the mouse axis, and a setting that can make consecutive dashes easier
    added a few new particle effects including one for the player's weapon
    created a sound system that will allow us to assign multiple different sounds that can play for the same actions
    added bouncy mushroom with animation
    set up the first stage so that there's a barrier blocking the way to the main tag and the player needs to get 4 other tags to make the barrier go away, and added UI that keeps track of the other tags collected
    made the main tag trigger the win screen when you pick it up hopefully without breaking anything
    fixed clicking in the pause menu making the player start an attack while still paused

---
+++ FinalProject-CouchPotatoStudio/Assets/Scripts/BounceMushroom.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceMushroom : MonoBehaviour
+{
+    public AudioSource bounceSound;
+    public Animator mushroomAnimator;
+    public float bounce;
+    private PlayerMovement player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] playerObjects = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject candidate in playerObjects)
+        {
+            if (candidate.GetComponent<PlayerMovement>() != null)
+            {
+                player = candidate.GetComponent<PlayerMovement>();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            player.velocity.y = bounce;
+            bounceSound.Play();
+            mushroomAnimator.SetTrigger("Bounce");
+        }
+    }
+}
---
+++ FinalProject-CouchPotatoStudio/Assets/Scripts/Stage1ProgressManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage1ProgressManager : MonoBehaviour
+{
+    public GameObject winScreen, gameUI, barrier;
+    private int tags = 0;
+    public UnityEngine.UI.Text objectiveText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Attack"))
+        {
+            return;
+        }
+        if (collider.CompareTag("KeyItem"))
+        {
+            if (collider.gameObject.GetComponent<KeyItem>().itemName == "ofuda")
+            {
+                Time.timeScale = 0f;
+                PauseController.Paused = true;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                gameUI.SetActive(false);
+                winScreen.SetActive(true);
+            }
+            if (collider.gameObject.GetComponent<KeyItem>().itemName == "tag")
+            {
+                tags++;
+                objectiveText.text = tags + "/4";
+                if (tags > 3)
+                {
+                    Destroy(barrier);
+                }
+            }
+            Destroy(collider.gameObject);
+        }
+    }
+}
